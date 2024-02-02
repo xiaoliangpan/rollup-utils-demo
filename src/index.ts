d2a526f331caa24ca492fc6def05919dba96d186
panxiaoliang <panxiaoliang>
2024-02-02 17:31:25 +0800

    fix: 修改入库文件部分代码

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,4 @@ export { subtract } from './subtract';
 
 const str = 'my name ispan xiao liang ta shi ge xiaohudd ddddfdfdf d dfdfdfd jddfdf jjjjjj ';
 
-const str1 = 'bbb';
 console.log(str, str1);
